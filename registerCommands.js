9b309b1ba9a971b65bb0c5368b213bc09c22c8cf
mak8907 <59779431+mak8907@users.noreply.github.com>
2025-09-25 08:38:10 -0700

    Update registerCommands.js

--- registerCommands.js
+++ registerCommands.js
@@ -1,105 +1,39 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const commands = [
-  {
-    name: 'join',
-    description: 'Join voice channel',
-    options: [
-      {
-        type: 3, // STRING
-        name: 'mode',
-        description: 'The mode to join in',
-        required: false,
-        choices: [
-          {
-            name: 'silent',
-            value: 'silent',
-            description: "The bot won't produce any sound in this mode (confirmation, processed, etc.)",
-          },
-          {
-            name: 'free',
-            value: 'free',
-            description: 'The bot does not need trigger word to talk and will respond to any voice input. This mode is not recommended for general use.',
-          },
-          {
-            name: 'transcribe',
-            value: 'transcribe',
-            description: 'The bot will save a transcription of the conversation and send it to the channel after using the `leave` command.',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: 'reset',
-    description: 'Reset voice chat history',
-  },
-  {
-    name: 'play',
-    description: 'Play a song from YouTube',
-    options: [
-      {
-        type: 3, // STRING
-        name: 'query',
-        description: 'The song name or URL to play',
-        required: true,
-      },
-    ],
-  },
-  {
-    name: 'search',
-    description: 'Search internet with LLM',
-    options: [
-      {
-        type: 3, // STRING
-        name: 'query',
-        description: 'Your query',
-        required: true,
-      },
-    ],
-  },
-  {
-    name: 'reminder',
-    description: 'Set a reminder',
-    options: [
-      {
-        type: 3, // STRING
-        name: 'time',
-        description: 'Time to remind, in timestamp format (e.g. <t:1715869560>)',
-        required: true,
-      },
-      {
-        type: 3, // STRING
-        name: 'message',
-        description: 'Message to remind',
-        required: true,
-      },
-    ],
-  },
-  {
-    name: 'leave',
-    description: 'Leave voice channel',
-  },
-  {
-    name: 'help',
-    description: 'Display help message',
-  },
-];
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('join')
+    .setDescription('Join voice channel'),
+  async execute(interaction) {
+    if (interaction.member.voice.channel) {
+      const connection = joinVoiceChannel({
+        channelId: interaction.member.voice.channel.id,
+        guildId: interaction.guild.id,
+        adapterCreator: interaction.guild.voiceAdapterCreator,
+        selfDeaf: false,
+      });
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+      logToConsole('> Joined voice channel', 'info', 1);
+      handleRecording(connection, interaction.member.voice.channel);
 
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
+      await interaction.reply({ content: 'Joined voice channel.', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'You need to join a voice channel first!', ephemeral: true });
+    }
+  },
+};
 
-    await rest.put(
-      Routes.applicationCommands(process.env.DISCORD_ID),
-      { body: commands },
-    );
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('leave')
+    .setDescription('Leave voice channel'),
+  async execute(interaction) {
+    if (connection) {
+      leaveVoiceChannel(connection);
+      logToConsole('> Left voice channel', 'info', 1);
+      await interaction.reply({ content: 'Left voice channel.', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'I am not in a voice channel!', ephemeral: true });
+    }
+  },
+};
